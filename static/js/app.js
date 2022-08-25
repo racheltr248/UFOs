9b85788c165487d20d9cdc03e511637e7afad5f5
Your Name <mmrtait@gmail.com>
2022-08-25 19:02:41 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,21 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // clear any existing data
+    tbody.html("");
+
+    // loop through each object in the data
+    // append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // function to append a table row to the table body
+        let row = tbody.append("tr");
+        // loop through each field in the dataRow
+        // add each value as a table cell
+        Object.values(dataRow).forEach((val) ==> {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
