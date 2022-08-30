f6030a1995dbdef6ce73e5a72089efb89045f91e
Your Name <mmrtait@gmail.com>
2022-08-30 19:00:11 -0400

    update challenge completed

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# UFOs
+# UFO Sightings Analysis
+
+## Overview of Project
+The stakeholder wants to create a dynamic webpage and table that allows users to filter for multiple criteria at the same time. 
+
+## Results
+Using Javascript, I created multiple filters that will allow users to limit their results by date, city, state, country, and/or shape. 
+
+The default format of the filters indicates that the user should enter the appropriate text. 
+
+![Default filters](./report_images/filters_default.PNG)
+
+
+In action, the filters can be used one at a time or together to produce desired results. 
+
+![Filters in use](./report_images/filters_in_use.PNG)
+
+## Summary
+While the final webpage is dynamic and provides the desired filters, there are some issues that could be rectified with additional development. 
+
+- The first issue is with formatting. It may not be evident to users that city, state, and country names must be entered in lowercase in order to return any values since that's the format of the data. Using the toLowerCase method should ensure that any case that the user implements is converted appropriately to lower case. 
+- The second, similar issue is with date formatting. Including dateControl in the code could prevent confusion for those who use a different date format. 
---
+++ static/js/app_module.js
@@ -0,0 +1,42 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // clear any existing data
+    tbody.html("");
+    // loop through each object in the data
+    // append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // function to append a table row to the table body
+        let row = tbody.append("tr");
+        // loop through each field in the dataRow
+        // add each value as a table cell
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
+
+// start setting up filters
+// date data
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if date filter is active and filter if so
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // rebuild table using filters if applicable
+    // return original tableData if no filters
+    buildTable(filteredData);
+}
+
+// event listener for button click
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page first loads
+buildTable(tableData);
